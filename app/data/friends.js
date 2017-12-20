9a43b44625ee8270bf7e4f9ea779bc0a2ee72378
Lonnie Laub <llaub01@gmail.com>
2017-12-19 23:06:43 -0600

    last one

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,68 @@
-var friends = [];
+var friends = [
+{
+	"name": "Timmy",
+	"photo": "https://vignette.wikia.nocookie.net/southpark/images/4/4b/Timmy.png/revision/latest/scale-to-width-down/509?cb=20171014061716",
+	"scores": [
+		"4",
+		"4",
+		"4",
+		"3",
+		"1",
+		"1",
+		"3",
+		"5",
+		"5",
+		"2"
+	]
+},
+{
+	"name": "Bart",
+	"photo": "https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20140826010339",
+	"scores": [
+		"1",
+		"4",
+		"3",
+		"3",
+		"1",
+		"2",
+		"3",
+		"5",
+		"2",
+		"2"
+	]
+},
+{
+	"name": "Gizmo",
+	"photo": "https://vignette.wikia.nocookie.net/gremlins/images/f/fa/Gizmo.PNG/revision/latest?cb=20090920192843",
+	"scores": [
+		"2",
+		"5",
+		"3",
+		"2",
+		"1",
+		"2",
+		"3",
+		"1",
+		"2",
+		"4"
+	]
+},
+{
+	"name": "Johnny5",
+	"photo": "https://vignette.wikia.nocookie.net/robotsupremacy/images/7/7f/Johnny5-2.jpg/revision/latest/scale-to-width-down/272?cb=20120321192157",
+	"scores": [
+		"3",
+		"5",
+		"3",
+		"2",
+		"2",
+		"2",
+		"3",
+		"1",
+		"2",
+		"4"
+	]
+}
+];
 
 module.exports = friends;
